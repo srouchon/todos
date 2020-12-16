dd893760a07e6cbe06bd1e238d2ffa3088eac87d
sophie rouchon <sophie.rouchon@gmail.com>
2020-12-16 10:59:48 +0100

    allow user to create todo

---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature "User creates todo" do
+  scenario "successfully" do
+    visit('/') #capibara syntax
+    click_on('Add a new todo') #capibara syntax
+    fill_in("Title",	with: "Buy almond milk") #capibara syntax
+    click_on('Submit') #capibara syntax
+    
+    expect(page).to have_css '.todos li', text: 'Buy almond milk' #rspec syntax
+  end
+end
